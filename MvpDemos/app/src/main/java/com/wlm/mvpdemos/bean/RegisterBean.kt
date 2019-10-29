2524bb3cd7d17d73bec8bf4576e7eb30958f0120
wlm <1062397463@qq.com>
2019-10-29 11:28:12 +0800

    mvpDemos提交

---
+++ MvpDemos/app/src/main/java/com/wlm/mvpdemos/bean/RegisterBean.kt
@@ -0,0 +1,7 @@
+package com.wlm.mvpdemos.bean
+
+data class RegisterBean(
+    val `data`: Data,
+    val errorCode: Int,
+    val errorMsg: String
+)
---
+++ MvpDemos/app/src/main/java/com/wlm/mvpdemos/mvp/presenter/LoginPresenter.kt
@@ -0,0 +1,2 @@
+package com.wlm.mvpdemos.mvp.presenter
+
